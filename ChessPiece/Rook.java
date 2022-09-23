9b31987af323220dc7a076bb8b56fa2817241ef0
CalebStadler <caleb.stadler1@gmail.com>
2022-09-23 11:47:37 -0500

    Rook Movement
    Moved the rook movement into a method in PieceAbstract.java and called
    that method within the Rook.java class in the move method.

--- ChessPiece/Rook.java
+++ ChessPiece/Rook.java
@@ -28,46 +28,6 @@ public class Rook extends PieceAbstract
     public void move(int x, int y, PieceAbstract[] pieces)
         throws InvalidMoveException
     {
-        boolean notBetween = true;
-        int occupyingPiece = -1;
-        //check vertically for pieces between
-        if((this.x == x && this.y != y) || (this.x != x && this.y == y))
-        {
-            for(int i = 0; i < pieces.length; i++)
-            {
-                //check vertically for pieces between
-                if(pieces[i].getX() == x)
-                {
-                    if((pieces[i].getY() < this.y && pieces[i].getY() > y) ||
-                        pieces[i].getY() < y && pieces[i].getY() > this.y)
-                    {
-                        notBetween = false;
-                    }
-                }
-                //check horizontally for pieces between
-                else if(pieces[i].getY() == y)
-                {
-                    if((pieces[i].getX() < this.x && pieces[i].getX() > x) ||
-                        pieces[i].getX() < x && pieces[i].getX() > this.x)
-                    {
-                        notBetween = false;
-                    }
-                }
-                //check if player is attempting to move ontop of their own piece
-                if(pieces[i].getPlayer() == this.getPlayer() &&
-                    pieces[i].getX() == x && pieces[i].getY() == y)
-                {
-                    occupyingPiece = i;
-                }
-            }
-        }
-        if(notBetween && occupyingPiece < 0 && (this.x == x || this.y == y))
-        {
-            this.x = x;
-            this.y = y;
-        }
-        else
-            throw new InvalidMoveException("Rooks move horizontally and vertically, "
-                +"and can't pass through other pieces.");
+        rookMove(x, y, pieces);
     }
 }
