add240a1d45abd75fb24427cafc4b51e784c62ad
CalebStadler <caleb.stadler1@gmail.com>
2022-09-29 15:54:35 -0500

    Implemented Castling
    Implemented castling in the move method of the King along with comments
    explaining how it all works.

--- ChessPiece/King.java
+++ ChessPiece/King.java
@@ -29,7 +29,7 @@ public class King extends PieceAbstract
         throws InvalidMoveException
     {
         int occupyingPiece = -1;
-
+        boolean notBetween = true;
         //if moving one space
         if(Math.abs(x - this.x) < 2 && Math.abs(y - this.y) < 2)
         {
@@ -42,7 +42,59 @@ public class King extends PieceAbstract
                 }
             }
         }
-        if(occupyingPiece < 0 && Math.abs(x - this.x) < 2 && Math.abs(y - this.y) < 2)
+        //if King has not moved and is trying to move two across the same x for castling
+        if(notMoved && (Math.abs(x - this.x) == 2 && y == this.y))
+        {
+            //iterating through the array to find the king piece
+            for(int i = 0; i < pieces.length; i++)
+            {
+                //if finding king
+                if(pieces[i].getX() == this.x && pieces[i].getY() == this.y)
+                {
+                    //if king is moving left and the left rook has not moved
+                    if(x - this.x == -2 && pieces[i - 3].getNotMoved())
+                    {
+                        //iterating through array checking if pieces are between rook and king
+                        for (int j = 0; j < pieces.length; j++)
+                        {
+                            if(pieces[j].getX() < this.x && pieces[j].getX() > this.x - 3 &&
+                            pieces[j].getY() == this.y)
+                            {
+                                notBetween = false;
+                            }
+                        }
+                        //if nothing is between move king over 2 spaces and rook next to it on the opposite side
+                        if (notBetween)
+                        {
+                            this.x = x;
+                            pieces[i - 3].setX(this.x + 1);
+                            notMoved = false;
+                        }
+                    }
+                    //if king is moving right and right rook has not moved 
+                    else if(x - this.x == 2 && pieces[i + 4].getNotMoved())
+                    {
+                        //iterating through array checking if pieces are between rook and king
+                        for (int j = 0; j < pieces.length; j++)
+                        {
+                            if(pieces[j].getX() > this.x && pieces[j].getX() < this.x + 4 &&
+                                pieces[j].getY() == this.y)
+                            {
+                                notBetween = false;
+                            }
+                        }
+                        //if nothing is between move king over 2 spaces and rook next to it on the opposite side
+                        if (notBetween)
+                        {
+                            this.x = x;
+                            pieces[i + 4].setX(this.x - 1);
+                            notMoved = false;
+                        }
+                    }
+                }
+            }
+        }
+        else if(occupyingPiece < 0 && Math.abs(x - this.x) < 2 && Math.abs(y - this.y) < 2)
         {
             this.x = x;
             this.y = y;
@@ -56,10 +108,6 @@ public class King extends PieceAbstract
         }
         else
             throw new InvalidMoveException("Kings can only move one space at a time, in any direction.");
-    }
-    public void castle(String direction)
-    {
-
     }
     public boolean check()
     {
