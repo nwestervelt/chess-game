d1be8effe83e9d5cec7986ed3bdd6e1cd02ee359
CalebStadler <caleb.stadler1@gmail.com>
2022-10-21 12:35:09 -0500

    Fixed Bug with Rook
    Rook was not reaching the exception whenever it tried to move on top of
    itself. This made the turn switch even though it was an invalid move.

--- PieceAbstract.java
+++ PieceAbstract.java
@@ -160,6 +160,9 @@ public abstract class PieceAbstract
                     occupyingPiece = i;
             }
         }
+        else
+            throw new InvalidMoveException("Rooks move horizontally and vertically, "
+                +"and can't pass through other pieces.");
         if(notBetween && occupyingPiece < 0 && (this.x == x || this.y == y))
         {
             this.x = x;
